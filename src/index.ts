84ef41a2d8f92327e83f17f4d754748e8ec75763
Vlad Leppa <leppavlad@gmail.com>
2021-10-15 16:41:42 +0400

    Initialized project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// import "./css/fromjs.sass";
+
+// async function asyncAwaitSample() {
+//   const { tes } = await import("./tes");
+//   const result = await tes();
+//   console.log(result);
+// }
+
+// asyncAwaitSample();
+
+console.log("Typescript file");
+// const a:number = "4";
